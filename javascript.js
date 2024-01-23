13930d0fc080a4dc6a74fbb310e58e4c48e3fe6a
ashtondummitt <ashtondummitt@gmail.com>
2024-01-23 14:27:09 -0500

    Set a prompt to get 'playerSelection'. Defined 'computerSelection' variable to equal getComputerChoice function. Utilizing console logs to provide accurate returns of stated functions and variables. Declared 'game()' function but have not input body details.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
+const playerSelection = prompt("Rock, Paper, or Scissors?")
+const computerSelection = getComputerChoice;
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
 
+
+function game(playerSelection, computerSelection) {
+ 
+}
+console.log(getComputerChoice());   
+console.log(playerSelection);
+console.log(game(playerSelection, computerSelection));
