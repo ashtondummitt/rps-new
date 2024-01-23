60b0181b91f240e9e951e85f4125a00f66c36310
ashtondummitt <ashtondummitt@gmail.com>
2024-01-22 23:07:58 -0500

    Introduced 'getComputerChoice' function. Selects a random string from array using Math.floor, Math.random, and .length. Confirmed functionality with console.log.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice());
+
