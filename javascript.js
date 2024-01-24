80b6766d47f2ce5ef783902fdf3bead442e450ec
ashtondummitt <ashtondummitt@gmail.com>
2024-01-24 15:48:55 -0500

    Implemented 'playRound' function to play single rounds between 'playerSelection' and 'computerSelection' parameters. Had issues with console returning incorrect results were due to incorrect console.log parameters. Issue is fixed and console is returning accurate results.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
-const playerSelection = prompt("Rock, Paper, or Scissors?")
-const computerSelection = getComputerChoice;
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
+    let random = choices[Math.floor(Math.random() * choices.length)];
+    return random;
 }
 
 
+function playRound(playerSelection, computerSelection) {
+  
+if (playerSelection === computerSelection) {  
+        return "You Tied";
 
-function game(playerSelection, computerSelection) {
- 
-}
-console.log(getComputerChoice());   
+} else if (playerSelection === "Rock"&& computerSelection === "Scissors" 
+ || playerSelection === "Paper" && computerSelection === "Rock" 
+ || playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win";
+
+} else if (playerSelection === "Rock"&& computerSelection === "Paper" 
+|| playerSelection === "Paper" && computerSelection === "Scissors" 
+|| playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You lose";
+
+} 
+};
+
+
+
+
+
+
+
+
+
+
+//const playerSelection = prompt("Rock, Paper, or Scissors?");
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
 console.log(playerSelection);
-console.log(game(playerSelection, computerSelection));
+console.log(computerSelection);   
