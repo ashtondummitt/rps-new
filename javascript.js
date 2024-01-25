d8d4190386859d14189638d79037e1074499a3c1
ashtondummitt <ashtondummitt@gmail.com>
2024-01-24 19:06:48 -0500

    Created 'game()' function to run game and loop five times to declare a winner. Function will return results of individual rounds but not yet developed to count rounds results to return 'best out of five'. I have been overcomplicating this project and using previous work through as a reference. Time to keep it simple and clean.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let random = choices[Math.floor(Math.random() * choices.length)];
@@ -6,26 +8,41 @@ function getComputerChoice() {
 }
 
 
+
 function playRound(playerSelection, computerSelection) {
   
 if (playerSelection === computerSelection) {  
-        return "You Tied";
+        alert("You Tied");
 
 } else if (playerSelection === "Rock"&& computerSelection === "Scissors" 
  || playerSelection === "Paper" && computerSelection === "Rock" 
  || playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You Win";
+    alert("You Win");
 
 } else if (playerSelection === "Rock"&& computerSelection === "Paper" 
 || playerSelection === "Paper" && computerSelection === "Scissors" 
 || playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You lose";
-
-} 
-};
-
+    alert("You lose");
+}
+}
 
 
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Rock, Paper, or Scissors?");
+
+    playRound(playerSelection, computerSelection);
+    }  
+}
+
+
+
+game();
+
+
+
+    
 
 
 
@@ -33,10 +50,8 @@ if (playerSelection === computerSelection) {
 
 
 
-//const playerSelection = prompt("Rock, Paper, or Scissors?");
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection);
-console.log(computerSelection);   
+
+
+
+  
